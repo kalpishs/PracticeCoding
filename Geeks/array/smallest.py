e488451c9e63625b0695924a1cc8a43208a7835b
kalpishs <kalpish.singhal@gmail.com>
2025-03-10 22:22:20 +0530

    Smallest subarray with sum greater than x

---
+++ Geeks/array/smallest.py
@@ -0,0 +1,42 @@
+#https://www.geeksforgeeks.org/problems/smallest-subarray-with-sum-greater-than-x5651/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign
+from typing import *
+
+class Solution:
+    def smallestSubWithSum(self, x:int, arr:list[int]):
+        high, low = 0, 0
+        sum = 0
+        result = float('inf')
+        n = len(arr)
+        while high < n:
+            sum += arr[high]
+            while sum > x:
+                result = min(result, high - low + 1)
+                sum -= arr[low]
+                low += 1
+            high += 1
+        if result == float('inf'):
+            return 0
+        return result
+        # Your code goes here
+
+
+# {
+# Driver Code Starts
+def main():
+    T = int(input())
+
+    while (T > 0):
+        x = int(input())
+        a = [int(x) for x in input().strip().split()]
+        print(Solution().smallestSubWithSum(x, a))
+
+        T -= 1
+        print("~")
+
+
+if __name__ =="__main__":
+    bS = Solution()
+    inputArr:list[int] = [2, 3, 4, 10, 40]
+    numberX:int = 55
+    result=bS.smallestSubWithSum(numberX,inputArr)
+    print(f"size of the smallest sub arr with sum >= {numberX} is {result}")
